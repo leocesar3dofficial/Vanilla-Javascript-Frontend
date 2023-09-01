f31574ca8505d55384b98f8dfe61852beb9769ee
leocesar3dofficial <leocesarbsb@gmail.com>
2023-08-31 23:45:03 -0300

    a1

--- js/articles.js
+++ js/articles.js
@@ -15,50 +15,39 @@ const getArticlesList = async () => {
   }
 };
 
-// getArticlesList().then((articles) => {
-//   if (articles) {
-//     articlesList = articles;
-//   } else {
-//     console.log("Failed to fetch articles.");
-//   }
-// });
-
 (async () => {
   try {
     articlesList = await getArticlesList();
-    console.log("Data available:", articlesList);
   } catch (error) {
     console.error("Error:", error);
   }
 })();
 
-function getArticle(articlePath) {
+async function getArticle(articlePath) {
   const articleDescription = articlesList.find(
     (article) => article.slug === articlePath
   );
 
-  if (articleDescription === "undefined") {
-    console.log("article not found, do something!");
+  if (typeof articleDescription === "undefined") {
+    console.log("Article not found, do something!");
     return;
   }
 
   const articleURL = `../articles/${articleDescription.filename}`;
 
-  return fetch(articleURL).then((response) => {
+  try {
+    const response = await fetch(articleURL);
+
     if (!response.ok) {
       throw new Error(`Network response was not ok: ${response.status}`);
     }
 
-    return response
-      .text()
-      .then((htmlContent) => {
-        const articleContainer = document.getElementById("article-container");
-        articleContainer.innerHTML = htmlContent;
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  });
+    const htmlContent = await response.text();
+    const articleContainer = document.getElementById("article-container");
+    articleContainer.innerHTML = htmlContent;
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
 }
 
 function renderArticlesList() {
