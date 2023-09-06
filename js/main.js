19c108975137f8ada37f579c1caf87bbbe67a84f
leocesar3dofficial <leocesarbsb@gmail.com>
2023-09-06 14:38:47 -0300

    minor

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
-// main.js
-import { handlePath } from "./router.js"; // Import the necessary functions from your modules
+import { handlePath } from "./router.js";
 import { enableThemes } from "./themes.js";
 
-// Listen for the 'DOMContentLoaded' event to ensure the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
-  // Call the handlePath function to initialize your app
   handlePath();
   enableThemes();
 });
