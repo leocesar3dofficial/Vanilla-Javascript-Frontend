903c33dc141c2f2bbbe4715e26fcabad393cfa1f
leocesar3dofficial <leocesarbsb@gmail.com>
2023-10-12 03:56:32 -0300

    removed articleCard

--- js/articles.js
+++ js/articles.js
@@ -1,4 +1,3 @@
-import setCard from './articleCard.js';
 import Pagination from './pagination.js';
 
 let articlesList = [];
@@ -15,6 +14,27 @@ function displayMessage(elementId, text) {
   const elementText = document.createTextNode(text);
   paragraph.appendChild(elementText);
   systemMessage.appendChild(paragraph);
+}
+
+function setCard(article) {
+  const dateModified = new Date(article.dateModified);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = dateModified.toLocaleDateString('pt-BR', options);
+  const htmlSnippet = `
+    <div class="article-card">
+      <a href="/artigo/${article.slug}">
+        <img src="${article.image}" alt="foto do artigo"/>
+      </a>
+      <div class="card-content">
+        <h2>${article.title}</h2>
+        <h3>${article.category}</h3>
+        <p>${article.description}</p>
+        <h4>${formattedDate}</h4>
+      </div>
+    </div>
+  `;
+
+  return htmlSnippet;
 }
 
 const getArticlesList = async () => {
